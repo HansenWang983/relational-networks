300ff919fee39ed1c3c813f7e721b7639bbc7f3a
wzh <wangzehao1998@126.com>
2022-12-04 22:01:39 -0500

    Update model.py

--- model.py
+++ model.py
@@ -96,7 +96,7 @@ class RN(BasicModel):
 
         ##(number of filters per object+coordinate of object)*2+question vector
         if self.state_desc != 0:
-            self.g_fc1 = nn.Linear(7*2+11, 1000)
+            self.g_fc1 = nn.Linear(6*2+11, 1000)
         else:
             self.g_fc1 = nn.Linear(258*2+11, 1000)
 
@@ -140,9 +140,9 @@ class RN(BasicModel):
             # x = (64 x 6 x 7)
             x = state
             mb = x.size()[0]
-            n_channels = x.size()[2]
-            d = x.size()[1]
-            x_flat = state
+            n_channels = x.size()[1]
+            d = x.size()[2]
+            x_flat = x.view(mb,n_channels,d).permute(0,2,1)
             
         else:
             x = self.conv(img) ## x = (64 x 256 x 5 x 5)
